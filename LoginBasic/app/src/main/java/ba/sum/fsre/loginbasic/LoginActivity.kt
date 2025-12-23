ea490d10c9fbc304bfdd644ddf365074d1c44aea
zmarusic <zeljkomarusic@hotmail.com>
2025-12-23 17:44:06 +0100

    master gitignore update

--- LoginBasic/app/src/main/java/ba/sum/fsre/loginbasic/LoginActivity.kt
+++ LoginBasic/app/src/main/java/ba/sum/fsre/loginbasic/LoginActivity.kt
@@ -12,7 +12,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
