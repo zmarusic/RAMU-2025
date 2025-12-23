547c0ce49fac6ff382812507e3069cf5041e7adb
zmarusic <zeljkomarusic@hotmail.com>
2025-12-23 17:25:14 +0100

    master gitignore update

--- LoginFirebase/app/build.gradle.kts
+++ LoginFirebase/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ba.sum.fsre.loginfirebase"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ba.sum.fsre.loginfirebase"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- LoginFirebase/app/src/main/java/ba/sum/fsre/loginfirebase/LoginActivity.kt
+++ LoginFirebase/app/src/main/java/ba/sum/fsre/loginfirebase/LoginActivity.kt
@@ -1,6 +1,5 @@
 package ba.sum.fsre.loginfirebase
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
